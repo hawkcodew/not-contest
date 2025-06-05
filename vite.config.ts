4eebf3ee891c43550f7d66f08ca49d08c7490141
hawk <hawk@no.pics>
2025-06-05 16:10:32 +0200

    fix: remove Buffer dependency to fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -2,33 +2,9 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import inject from '@rollup/plugin-inject';
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    inject({
-      Buffer: ['buffer', 'Buffer'],
-    }),
-  ],
-  define: {
-    global: 'window',
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: 'globalThis'
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-      ]
-    }
-  },
+  plugins: [react(), tailwindcss()],
   assetsInclude: ['**/*.lottie'],
   resolve: {
     alias: {
